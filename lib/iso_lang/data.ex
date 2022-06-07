0abd004a59bf0c6270ace1410d3a7dee3d350b16
Everett Griffiths <everett@fireproofsocks.com>
2022-06-07 17:28:55 -0400

    Fixes CSV parsing to handle quotes

--- lib/iso_lang/data.ex
+++ lib/iso_lang/data.ex
@@ -9,18 +9,36 @@ defmodule IsoLang.Data do
     File.stream!("priv/language-codes-full.csv")
     |> Stream.drop(1)
 
+  # enm,,,"English, Middle (1100-1500)",anglais moyen (1100-1500)
+  # ypk,,,Yupik languages,"yupik, langues"
+  # Regex.named_captures(~r/^(?<alpha3b>.*),.*/U, line)
+  # "\"English, Middle (1100-1500)\",anglais moyen (1100-1500)"
+  # <<?", tail::binary>>
+  #  Regex.run(~r/(.*)",(.*)/, "English, Middle (1100-1500)\",anglais moyen (1100-1500)", capture: :all_but_first)
+
   langs =
     Enum.map(lines, fn l ->
-      [alpha3b, alpha3t, alpha2, english_name | _] =
+      [alpha3b, alpha3t, alpha2 | rest] =
         l
         |> String.trim()
         |> String.split(",")
+
+      tail = Enum.join(rest, ",")
+
+      # Parse up to the closing double-quote or up to the comma
+      f = fn
+        <<?", chunk::binary>> ->
+          Regex.run(~r/(.*)"/U, chunk, capture: :all_but_first) |> Enum.at(0)
+
+        chunk ->
+          Regex.run(~r/(.*),/U, chunk, capture: :all_but_first) |> Enum.at(0)
+      end
 
       %IsoLang{
         alpha2: alpha2,
         alpha3b: alpha3b,
         alpha3t: alpha3t,
-        name: english_name
+        name: f.(tail)
       }
     end)
 
